1230a026676b169f36ab32e81fb62f0ef12d4260
Riku Virtanen <virtanen.riku91@gmail.com>
2023-03-02 12:22:36 +0200

    PR review changes

--- Frends.PDF.Create/CHANGELOG.md
+++ Frends.PDF.Create/CHANGELOG.md
@@ -3,7 +3,7 @@
 ## [1.0.1] - 2023-03-01
 ### Fixed
 - Fixed document link to task portal. 
-_ Added requirements to summary.
+- Added requirements to summary.
 
 ## [1.0.0] - 2022-05-10
 ### Added
--- Frends.PDF.Create/Frends.PDF.Create/Create.cs
+++ Frends.PDF.Create/Frends.PDF.Create/Create.cs
@@ -6,7 +6,6 @@ using MigraDoc.Rendering;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
